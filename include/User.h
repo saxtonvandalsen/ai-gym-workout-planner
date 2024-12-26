bb003e8655a7d775f7fccaac818be136867b5254
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-12-26 10:04:02 -0800

    add prelim data

---
+++ include/User.h
@@ -0,0 +1,17 @@
+
+#include <string>
+using namespace std;
+
+namespace UserLibrary {
+
+    class User {
+        private:
+            string sex;
+            int height;
+            double weight;
+            int fitnessLevel;
+
+        public:
+            User();
+    };
+}
