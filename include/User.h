850642c47a153e1ac1607cc8b672b36314dd3b56
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-12-31 10:31:39 -0800

    Refactor User class with private validation methods and data attributes

--- include/User.h
+++ include/User.h
@@ -3,23 +3,46 @@
 #define USER_H
 
 #include <string>
+#include <vector>
 using namespace std;
 
 namespace UserLibrary {
 
     class User {
         private:
+            // Personal Information
             string name;
             string sex;
             int height;
             double weight;
-            int fitnessLevel;
+            vector<string> goals;
 
+            // Health & Progress Tracking
+            double bodyFatPercentage;
+            string trainingExperience;
+            vector<double> weightLog;
+            vector<double> strengthLog;
+            
+            // Dietary Preference & Tracking
+            string dietaryPreference;
+            int dailyCalories;
+            int dailyProteinTarget;
+            int dailyCarbTarget;
+            int dailyFatTarget;
+
+            void validateWeight(double weight);
+            void validateHeight(int height);
+            void validateStringInfo(string s);
+            void validateGoals(vector<string> goals);
+            void validatePersonalInfo();
             void calculateCaloricNeeds();
             void trackProgress();
             void updatedUserStats();
         public:
-            User();
+            User(string name, string sex, int height, int weight, vector<string> goals);
+            // Copy & move semantics eventually
+
+            string viewProgress();
     };
 }
 
