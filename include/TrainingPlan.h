6bc2d930ba1e550c3d02debe9fecdc5a1855a9c8
saxtonvandalsen <saxtonvandalsen@gmail.com>
2025-01-02 12:09:47 -0800

    first draft on training plan foundation

--- include/TrainingPlan.h
+++ include/TrainingPlan.h
@@ -1,11 +1,22 @@
 
 
+#include <string>
+#include <vector>
+using namespace std;
+
 namespace TrainingPlanLibrary {
 
     class TrainingPlan {
         private:
+            string planName;
+            string goal;
+            int durationWeeks;
+            string difficultyLevel;
+            vector<string> targetMuscleGroups;
 
         public:
-        
+            TrainingPlan();
+            TrainingPlan(const string& name, const string& goal, const string& difficulty, int workoutsPerWeek);
+            TrainingPlan(const TrainingPlan& other);
     };
 }
